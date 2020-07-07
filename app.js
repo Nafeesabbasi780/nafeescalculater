cf97b28d0b12f18d8ef11ae25d1ccd92d46af4e3
Nafeesabbasi780 <65039852+Nafeesabbasi780@users.noreply.github.com>
2020-07-07 21:44:44 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,25 @@
+function getNumber(num){
+
+    var result = document.getElementById("result");
+   
+    result.value += num;
+}
+function clearResult(){
+    var result = document.getElementById("result");
+
+    result.value = " "
+}
+
+
+
+
+function allResult(){
+    var result = document.getElementById("result");
+
+    result.value =  eval(result.value)
+}
+function back(){
+    var result = document.getElementById("result");
+
+    result.value =  result.value.slice(0,-1)
+}
